6cc69076b8b0ffa11b55b5465029d306d10c01bb
Leonardzin <Leonardzin@gmail.com>
2014-04-21 15:57:31 -0400

    sec branch

--- src/main/java/org/apache/lucene/analysis/CustomAnalyzerWrapper.java
+++ src/main/java/org/apache/lucene/analysis/CustomAnalyzerWrapper.java
@@ -69,7 +69,7 @@ public abstract class CustomAnalyzerWrapper extends Analyzer {
 
     @Override
     public int getOffsetGap(String fieldName) {
-        System.out.println("Test Branch!");
+        System.out.println("Branch second time!");
         return getWrappedAnalyzer(fieldName).getOffsetGap(fieldName);
     }
 
